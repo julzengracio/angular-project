c8bdd7a1a3c9c8071e2c31cb95227dca2df694f4
Jullian Engracio <N00145647@iadt.ie>
2018-01-31 11:22:22 +0000

    first commit

---
+++ server/routes/api.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+//const MongoClient = require('mongodb').MongoClient;
+const article = require('../models/article');
+
+const url = "mongodb://@localhost:27017";
+
+let db;
+
+// Connect to server
+mongoose.Promise = global.Promise;
+mongoose.connect(url, function(err, database) {
+    if(err){
+        console.log('Error connecting')
+        console.log(err);
+        return;
+    }
+    db = database;
+    console.log("Connected successfully to server");
+    //server.listen(8888);
+});
+
+router.get('/all', function(req, res) {
+    article.find({})
+        .exec(function(err, articles) {
+            if (err) {
+                console.log('Error getting the articles');
+                console.log(err);
+            } else {
+                //console.log(articles);
+                res.json(articles);
+            }
+        });
+});
+
+router.get('/articles/:id', function(req, res) {
+    console.log('Requesting a specific article');
+    article.findById(req.params.id)
+        .exec(function(err, article){
+            if(err){
+                console.log('Error getting the article')
+                console.log(err);
+            } else {
+                res.json(article);
+            }
+        });
+});
+
+router.post('/create', function(req, res) {
+    console.log('Posting an Article');
+    var newArticle = new article();
+    newArticle.title = req.body.title;
+    newArticle.content = req.body.content;
+    newArticle.save(function(err, article) {
+        if(err) {
+            console.log('Error inserting the article');
+            console.log(err);
+        } else {
+            res.json(article);
+        }
+    });
+});
+
+router.post('/update/:id', function(req, res) {
+    console.log('Updating an Article');
+
+    article.findById(req.params.id)
+        .exec(function(err, article) {
+            if (err) {
+                console.log('Could not find the article');
+            } else {
+                article.title = req.body.title;
+                article.content = req.body.content;
+                article.save();
+                res.json(article);
+            }
+        });
+});
+
+router.get('/delete/:id', function(req, res) {
+    console.log('Deleting an article');
+    article.findByIdAndRemove(req.params.id)
+        .exec(function(err, article){
+            if(err){
+                console.log('Error deleting the article')
+                console.log(err);
+            } else {
+                res.json(article);
+            }
+        });
+});
+
+module.exports = router;
